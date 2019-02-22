fe8d122ec53ca0594e39ccfcf275ec709aee4e81
Nikola Hristov <nikolahr0707@icloud.com>
2019-02-22 17:10:58 +0200

    added comments and made person not visible

--- setperson/main.go
+++ setperson/main.go
@@ -2,23 +2,23 @@ package main
 
 import "fmt"
 
-type Person struct {
+type person struct {
 	name string
 	age int
 }
-
-func (v Person) SetName(newName string) string{
+//set age of person
+func (v person) SetName(newName string) string{
 	v.name=newName
 	return v.name
 }
-
-func (v *Person) SetAge(newAge int) int{
+//set name of person
+func (v *person) SetAge(newAge int) int{
 	v.age=newAge
     return v.age
 }
 
 func main()  {
-	v:=Person{"Nikola",21}
+	v:=person{"Nikola",21}
 	fmt.Println(v)
 	v.SetName("Pesho")
 	v.SetAge(19)
