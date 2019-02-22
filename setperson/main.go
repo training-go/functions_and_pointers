7461d1f23b0924f3628d97a3df829403fd3b1648
Nikola Hristov <nikolahr0707@icloud.com>
2019-02-22 17:28:15 +0200

    formated code

--- setperson/main.go
+++ setperson/main.go
@@ -4,21 +4,23 @@ import "fmt"
 
 type person struct {
 	name string
-	age int
-}
-//set age of person
-func (v person) SetName(newName string) string{
-	v.name=newName
-	return v.name
-}
-//set name of person
-func (v *person) SetAge(newAge int) int{
-	v.age=newAge
-    return v.age
+	age  int
 }
 
-func main()  {
-	v:=person{"Nikola",21}
+//set age of person
+func (v person) SetName(newName string) string {
+	v.name = newName
+	return v.name
+}
+
+//set name of person
+func (v *person) SetAge(newAge int) int {
+	v.age = newAge
+	return v.age
+}
+
+func main() {
+	v := person{"Nikola", 21}
 	fmt.Println(v)
 	v.SetName("Pesho")
 	v.SetAge(19)
