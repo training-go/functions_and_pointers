3b2f79ab7cec22b2e426c5ead8339b24fc40fa94
Nikola Hristov <nikolahr0707@icloud.com>
2019-02-25 14:52:02 +0200

    exported and added a comment to the person struct

--- setperson/main.go
+++ setperson/main.go
@@ -2,25 +2,26 @@ package main
 
 import "fmt"
 
-type person struct {
+//Contains the details of the person
+type Person struct {
 	name string
 	age  int
 }
 
 //set age of person
-func (v person) SetName(newName string) string {
+func (v Person) SetName(newName string) string {
 	v.name = newName
 	return v.name
 }
 
 //set name of person
-func (v *person) SetAge(newAge int) int {
+func (v *Person) SetAge(newAge int) int {
 	v.age = newAge
 	return v.age
 }
 
 func main() {
-	v := person{"Nikola", 21}
+	v := Person{"Nikola", 21}
 	fmt.Println(v)
 	v.SetName("Pesho")
 	v.SetAge(19)
