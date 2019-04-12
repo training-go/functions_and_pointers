05966ffb2718496ff7d121e38ed96268df36812d
Nikola Hristov <nikolahr0707@icloud.com>
2019-04-12 17:52:43 +0300

    removed uneeded code

--- setperson/main_test.go
+++ setperson/main_test.go
@@ -6,9 +6,6 @@ import "testing"
 
 func TestPerson_SetName(t *testing.T) {
 	v := Person{"Nikola", 21}
-	if v.Name!="Nikola" || v.Age!=21{
-		t.Errorf("There is a problem with the struct, want name: %s age : %d, got name: %s age : %d",v.Name,v.Age,"Nikola",21)
-	}
 	v.SetName("Pesho")
 	if v.Name!="Nikola"{
 		t.Errorf("The name has change, but it should't, want %s, got %s","Nikola",v.Name)
@@ -17,9 +14,6 @@ func TestPerson_SetName(t *testing.T) {
 
 func TestPerson_SetAge(t *testing.T) {
 	v := Person{"Nikola", 21}
-	if v.Name!="Nikola" || v.Age!=21{
-		t.Errorf("There is a problem with the struct, want name: %s age : %d, got name: %s age : %d",v.Name,v.Age,"Nikola",21)
-	}
 	v.SetAge(25)
 	if v.Age!=25{
 		t.Errorf("The age was not set, want %d, got %d",25,v.Age)
