49c864cc69ad7a1ed0e78d585f445fd7a8304e47
Nikola Hristov <nikolahr0707@icloud.com>
2019-04-15 15:36:34 +0300

    formated code

--- setperson/main_test.go
+++ setperson/main_test.go
@@ -2,20 +2,18 @@ package main
 
 import "testing"
 
-
-
 func TestPerson_SetName(t *testing.T) {
 	v := Person{"Nikola", 21}
 	v.SetName("Pesho")
-	if v.Name!="Nikola"{
-		t.Errorf("The name has change, but it should't, want %s, got %s","Nikola",v.Name)
+	if v.Name != "Nikola" {
+		t.Errorf("The name has change, but it should't, want %s, got %s", "Nikola", v.Name)
 	}
 }
 
 func TestPerson_SetAge(t *testing.T) {
 	v := Person{"Nikola", 21}
 	v.SetAge(25)
-	if v.Age!=25{
-		t.Errorf("The age was not set, want %d, got %d",25,v.Age)
+	if v.Age != 25 {
+		t.Errorf("The age was not set, want %d, got %d", 25, v.Age)
 	}
 }
--- sum/main_test.go
+++ sum/main_test.go
@@ -4,15 +4,15 @@ import (
 	"testing"
 )
 
-func TestSum(t *testing.T)  {
-	if Sum(5, 9, 2, -5, 6) != 17{
-		t.Errorf("Sum was not corrent, want %d, got %d",17,Sum(5, 9, 2, -5, 6))
+func TestSum(t *testing.T) {
+	if Sum(5, 9, 2, -5, 6) != 17 {
+		t.Errorf("Sum was not corrent, want %d, got %d", 17, Sum(5, 9, 2, -5, 6))
 	}
-	if Sum(2, 1, -222) != -219{
-		t.Errorf("Sum was not corrent, want %d, got %d",-219,Sum(2, 1, -222))
+	if Sum(2, 1, -222) != -219 {
+		t.Errorf("Sum was not corrent, want %d, got %d", -219, Sum(2, 1, -222))
 	}
 	s := []int{3, 6, 10, 4, -5, -9, 5, 77, 2, 5, 6, 21, 34, 52, -29}
-	if Sum(s...) != 182{
-		t.Errorf("Sum was not corrent, want %d, got %d",182,Sum(s...))
+	if Sum(s...) != 182 {
+		t.Errorf("Sum was not corrent, want %d, got %d", 182, Sum(s...))
 	}
 }
